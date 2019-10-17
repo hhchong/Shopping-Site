5af888f96f822d3c5c3c0f345015a74b89709231
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-17 12:11:23 -0700

    Fixed show_melon routes

--- shoppingsite.py
+++ shoppingsite.py
@@ -48,7 +48,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     print(melon)
     return render_template("melon_details.html",
                            display_melon=melon)
